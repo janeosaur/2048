389d316319c01cede4a962d71679ab1eaa562eeb
Jane Wie <jwie@skillz.com>
2017-09-15 16:31:17 -0700

    Core Skillz Implementation w/ custom theme.

--- m2048/Controller/M2ViewController.m
+++ m2048/Controller/M2ViewController.m
@@ -12,45 +12,29 @@
 #import "M2Scene.h"
 #import "M2GameManager.h"
 #import "M2ScoreView.h"
-#import "M2Overlay.h"
 #import "M2GridView.h"
 
 #import <Skillz/Skillz.h>
 
 @implementation M2ViewController {
-  IBOutlet UIButton *_multiplayerButton;
-  IBOutlet UIButton *_restartButton;
-  IBOutlet UIButton *_settingsButton;
+  IBOutlet UIButton *_abortButton;
   IBOutlet UILabel *_targetScore;
   IBOutlet UILabel *_subtitle;
   IBOutlet M2ScoreView *_scoreView;
   IBOutlet M2ScoreView *_bestView;
   
   M2Scene *_scene;
-  
-  IBOutlet M2Overlay *_overlay;
-  IBOutlet UIImageView *_overlayBackground;
 }
 
 - (void)viewDidLoad
 {
   [super viewDidLoad];
-  
+    
+  [[Skillz skillzInstance] launchSkillz];
+
   [self updateState];
   
   _bestView.score.text = [NSString stringWithFormat:@"%ld", (long)[Settings integerForKey:@"Best Score"]];
-    
-  _multiplayerButton.layer.cornerRadius = [GSTATE cornerRadius];
-  _multiplayerButton.layer.masksToBounds = YES;
-  
-  _restartButton.layer.cornerRadius = [GSTATE cornerRadius];
-  _restartButton.layer.masksToBounds = YES;
-  
-  _settingsButton.layer.cornerRadius = [GSTATE cornerRadius];
-  _settingsButton.layer.masksToBounds = YES;
-  
-  _overlay.hidden = YES;
-  _overlayBackground.hidden = YES;
     
     SKView * skView = (SKView *)self.view;
     
@@ -101,21 +85,11 @@
     NSLog(@"Skillz exited.");
 }
 
-
 - (void)updateState
 {
   [_scoreView updateAppearance];
   [_bestView updateAppearance];
-  
-  _restartButton.backgroundColor = [GSTATE buttonColor];
-  _restartButton.titleLabel.font = [UIFont fontWithName:[GSTATE boldFontName] size:14];
-    
-  _multiplayerButton.backgroundColor = [GSTATE buttonColor];
-  _multiplayerButton.titleLabel.font = [UIFont fontWithName:[GSTATE boldFontName] size:14];
-  
-  _settingsButton.backgroundColor = [GSTATE buttonColor];
-  _settingsButton.titleLabel.font = [UIFont fontWithName:[GSTATE boldFontName] size:14];
-  
+
   _targetScore.textColor = [GSTATE buttonColor];
   
   long target = [GSTATE valueForLevel:GSTATE.winningLevel];
@@ -133,14 +107,7 @@
   _subtitle.textColor = [GSTATE buttonColor];
   _subtitle.font = [UIFont fontWithName:[GSTATE regularFontName] size:14];
   _subtitle.text = [NSString stringWithFormat:@"Join the numbers to get to %ld!", target];
-  
-  _overlay.message.font = [UIFont fontWithName:[GSTATE boldFontName] size:36];
-  _overlay.keepPlaying.titleLabel.font = [UIFont fontWithName:[GSTATE boldFontName] size:17];
-  _overlay.restartGame.titleLabel.font = [UIFont fontWithName:[GSTATE boldFontName] size:17];
-  
-  _overlay.message.textColor = [GSTATE buttonColor];
-  [_overlay.keepPlaying setTitleColor:[GSTATE buttonColor] forState:UIControlStateNormal];
-  [_overlay.restartGame setTitleColor:[GSTATE buttonColor] forState:UIControlStateNormal];
+
 }
 
 
@@ -161,25 +128,19 @@
 }
 
 
-- (IBAction)multiplayerButtonPressed:(id)sender
+- (IBAction)abortButtonPressed:(id)sender
 {
-  [[Skillz skillzInstance] launchSkillz];
-  [_multiplayerButton removeFromSuperview];
-  [_restartButton removeFromSuperview];
-  [_settingsButton removeFromSuperview];
-}
-
-- (IBAction)restart:(id)sender
-{
-  [self hideOverlay];
-  [self updateScore:0];
-  [_scene startNewGame];
+        [[Skillz skillzInstance] notifyPlayerAbortWithCompletion:^() {
+            [self updateScore:0];
+            [_scene startNewGame];
+            NSLog(@"Player is forfeiting the game");
+        }];
 }
 
 
 - (IBAction)keepPlaying:(id)sender
 {
-  [self hideOverlay];
+  // [self hideOverlay];
 }
 
 
@@ -197,17 +158,11 @@
 
 - (void)endGame:(BOOL)won
 {
-  _overlay.hidden = NO;
-  _overlay.alpha = 0;
-  _overlayBackground.hidden = NO;
-  _overlayBackground.alpha = 0;
-  
+ 
   if (!won) {
-    _overlay.keepPlaying.hidden = YES;
-    _overlay.message.text = @"Game Over";
+   //    _overlay.message.text = @"Game Over";
   } else {
-    _overlay.keepPlaying.hidden = NO;
-    _overlay.message.text = @"You Win!";
+  //     _overlay.message.text = @"You Win!";
   }
   
     
@@ -221,38 +176,7 @@
                                                         NSLog(@"Reporting score to Skillz…");
                                                     }];
     }
-  // Fake the overlay background as a mask on the board.
-  _overlayBackground.image = [M2GridView gridImageWithOverlay];
-  
-  // Center the overlay in the board.
-  CGFloat verticalOffset = [[UIScreen mainScreen] bounds].size.height - GSTATE.verticalOffset;
-  NSInteger side = GSTATE.dimension * (GSTATE.tileSize + GSTATE.borderWidth) + GSTATE.borderWidth;
-  _overlay.center = CGPointMake(GSTATE.horizontalOffset + side / 2, verticalOffset - side / 2);
-  
-  [UIView animateWithDuration:0.5 delay:1.5 options:UIViewAnimationOptionCurveEaseInOut animations:^{
-    _overlay.alpha = 1;
-    _overlayBackground.alpha = 1;
-  } completion:^(BOOL finished) {
-    // Freeze the current game.
-    ((SKView *)self.view).paused = YES;
-  }];
 }
-
-
-- (void)hideOverlay
-{
-  ((SKView *)self.view).paused = NO;
-  if (!_overlay.hidden) {
-    [UIView animateWithDuration:0.5 animations:^{
-      _overlay.alpha = 0;
-      _overlayBackground.alpha = 0;
-    } completion:^(BOOL finished) {
-      _overlay.hidden = YES;
-      _overlayBackground.hidden = YES;
-    }];
-  }
-}
-
 
 - (void)didReceiveMemoryWarning
 {
